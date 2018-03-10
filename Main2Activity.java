46d7d6ab9f051eddf5852c6d38e51990679189b4
EricNguyenB <enguyen095@gmail.com>
2018-03-09 21:56:35 -0600

    Initial commit

---
+++ Main2Activity.java
@@ -0,0 +1,63 @@
+package com.tamo.timeandmanagementorganizer;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.delaroystudios.todolist.R;
+import com.delaroystudios.todolist.data.TaskContract;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    // Declare a member variable to keep track of a task's selected mPriority
+    private int mPriority;
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Main2Activity);
+
+        // Initialize to highest mPriority by default (mPriority = 1)
+        ((RadioButton) findViewById(R.id.make_event_button)).setChecked(true);
+        mPriority = 1;
+    }
+
+
+    /**
+     * onClickAddTask is called when the "ADD" button is clicked.
+     * It retrieves user input and inserts that new task data into the underlying database.
+     */
+    public void onClickAddTask(View view) {
+        // Not yet implemented
+        // Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        // If the EditText input is empty -> don't create an entry
+        String input = ((EditText) findViewById(R.id.enter_event)).getText().toString();
+        if (input.length() == 0) {
+            return;
+        }
+
+        // Insert new task data via a ContentResolver
+        // Create new empty ContentValues object
+        ContentValues contentValues = new ContentValues();
+        // Put the task description and selected mPriority into the ContentValues
+        contentValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, input);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
+        // Insert the content values via a ContentResolver
+        Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
+
+        // Display the URI that's returned with a Toast
+        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
+        if (uri != null) {
+            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        // Finish activity (this returns back to MainActivity)
+        finish();
+
+    }
+}
